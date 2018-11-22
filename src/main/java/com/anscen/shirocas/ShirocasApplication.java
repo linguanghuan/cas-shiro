1107c717ddc598537686c1895e12c32f00389d65
linguanghuan <linguanghuan@gmail.com>
2018-11-22 16:37:29 +0800

    spring boot shiro cas init compile ok

---
+++ src/main/java/com/anscen/shirocas/ShirocasApplication.java
@@ -0,0 +1,12 @@
+package com.anscen.shirocas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirocasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShirocasApplication.class, args);
+    }
+}
---
+++ src/test/java/com/anscen/shirocas/ShirocasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anscen.shirocas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShirocasApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
